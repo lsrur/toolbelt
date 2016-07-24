e9a49d90bbf95846b5af1fafafde722a1733d49c
Lautaro Srur <lautarosrur@MacBook-Pro-de-Lautaro.local>
2016-07-24 18:11:17 -0300

    fixes

--- src/Toolbelt.php
+++ src/Toolbelt.php
@@ -21,33 +21,6 @@ class Toolbelt
         return array_keys($keys) !== $keys;
     }
 
-
-    public function parse_phpinfo() {
-        ob_start(); phpinfo(); $s = ob_get_contents(); ob_end_clean();
-        $s = strip_tags($s, '<h2><th><td>');
-        $s = preg_replace('/<th[^>]*>([^<]+)<\/th>/', '<info>\1</info>', $s);
-        $s = preg_replace('/<td[^>]*>([^<]+)<\/td>/', '<info>\1</info>', $s);
-        $t = preg_split('/(<h2[^>]*>[^<]+<\/h2>)/', $s, -1, PREG_SPLIT_DELIM_CAPTURE);
-        $r = array(); $count = count($t);
-        $p1 = '<info>([^<]+)<\/info>';
-        $p2 = '/'.$p1.'\s*'.$p1.'\s*'.$p1.'/';
-        $p3 = '/'.$p1.'\s*'.$p1.'/';
-        for ($i = 1; $i < $count; $i++) {
-            if (preg_match('/<h2[^>]*>([^<]+)<\/h2>/', $t[$i], $matchs)) {
-                $name = trim($matchs[1]);
-                $vals = explode("\n", $t[$i + 1]);
-                foreach ($vals AS $val) {
-                    if (preg_match($p2, $val, $matchs)) { // 3cols
-                        $r[$name][trim($matchs[1])] = array(trim($matchs[2]), trim($matchs[3]));
-                    } elseif (preg_match($p3, $val, $matchs)) { // 2cols
-                        $r[$name][trim($matchs[1])] = trim($matchs[2]);
-                    }
-                }
-            }
-        }
-        return $r;
-    }
-
     /**
      * Returns current microtime 
      * @return float
@@ -58,6 +31,11 @@ class Toolbelt
         return ((float)$usec + (float)$sec);
     }
 
+    /**
+     * [isJsonRequest description]
+     * @param  [type]  $request [description]
+     * @return boolean          [description]
+     */
     public function isJsonRequest($request = null)
     {
     	$request = $request ?: request();
@@ -66,36 +44,6 @@ class Toolbelt
         }
         $acceptable = $request->getAcceptableContentTypes();
         return (isset($acceptable[0]) && $acceptable[0] == 'application/json');
-    }
-
-    public function getDump($v)
-    {
-        $styles = [
-            'default' => 'background-color:white; color:#222; line-height:1.2em; font-weight:normal; font:13px Monaco, Consolas, monospace; word-wrap: break-word; white-space: pre-wrap; position:relative; z-index:100000; border:0',
-            'num' => 'color:#a71d5d',
-            'const' => 'color:#795da3',
-            'str' => 'color:#df5000',
-            'cchr' => 'color:#222',
-            'note' => 'color:#a71d5d',
-            'ref' => 'color:#a0a0a0',
-            'public' => 'color:#795da3',
-            'protected' => 'color:#795da3',
-            'private' => 'color:#795da3',
-            'meta' => 'color:#b729d9',
-            'key' => 'color:#df5000',
-            'index' => 'color:#a71d5d',
-        ];
-        ob_start();
-
-        $dumper = new \Symfony\Component\VarDumper\Dumper\HtmlDumper;
-        $dumper->setStyles($styles);
-  
-        $dumper->dump((new \Symfony\Component\VarDumper\Cloner\VarCloner)->cloneVar($v));
-        
-        //$dumper->dump($v);
-        $result = ob_get_clean();
-    
-        return $result;    
     }
     
     /**
@@ -116,7 +64,7 @@ class Toolbelt
     }
 
     /**
-     * Add slash at the end if doesnt exists 
+     * Add slash at the end if doesnt exist
      * @param  [type] $str [description]
      * @return [type]      [description]
      */
@@ -125,30 +73,5 @@ class Toolbelt
 	    return substr($str,-1) !== '/' ? $str .'/' : $str;
 	}
 
-	public function varToHtml($var='', $key='') {
-	     $type = gettype($var);
-	      $result = '';
-
-	      if (in_array($type, array('object','array'))) {
-	        $result .= '
-	          <table class="debug-table">
-	            <tr>
-	              <td class="debug-key-cell"><b>'.$key.'</b><br/>Type: '.$type.'<br/>Length: '.count($var).'</td>
-	              <td class="debug-value-cell">';
-
-	        foreach ($var as $akey => $val) {
-	          $result .= $this->varToHtml($val, $akey);
-	        }
-	        $result .= '</td></tr></table>';
-	      } else {
-	        $result .= '<div class="debug-item"><span class="debug-label">'.$key.' ('.$type.'): </span><span class="debug-value">'.$var.'</span></div>';
-	      }
-
-	      return $result;
-	   }
-
-	    public function test()
-	    {
-	    	return "hello";
-	    }
-	}
+	
+}
